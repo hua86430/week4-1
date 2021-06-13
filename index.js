5726138880de4900b4cc92077e76e2f2a31c9a5e
hua86430 <k0981465867@gmail.com>
2021-06-13 21:54:53 +0800

    修改登入寫法

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
-const usernameInput = document.querySelector("#username");
-const passwordInput = document.querySelector("#password");
-const loginBtn = document.querySelector("#loginBtn");
-const url = 'https://vue3-course-api.hexschool.io/';
-
-loginBtn.addEventListener('click', getLoginData);
 
 
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
 
-function getLoginData () {
-  const username = usernameInput.value;
-  const password = passwordInput.value;
-  const user = {
-    username,
-    password
-  };
-  axios.post(`${url}admin/signin`, user)
-    .then(res => {
-      if (res.data.success) {
-        const token = res.data.token;
-        const expired = res.data.expired;
-        document.cookie = `huaToken=${token}; expires=${new Date(expired)}`;
-        window.location = "admin.html";
-      } else {
-        swal(`${res.data.message}`, '帳號或密碼錯誤，請重新輸入', 'error');
-      }
-    });
-}
+createApp({
+  data () {
+    return {
+      user: {
+        username: "",
+        password: ""
+      },
+      url: 'https://vue3-course-api.hexschool.io/'
+    };
+  },
+  methods: {
+    login () {
+      axios.post(`${this.url}admin/signin`, this.user)
+        .then(res => {
+          if (res.data.success) {
+            const { token, expired } = res.data;
+            document.cookie = `huaToken=${token}; expires=${new Date(expired)}`;
+            window.location = "admin.html";
+          } else {
+            swal(`${res.data.message}`, '帳號或密碼錯誤，請重新輸入', 'error');
+          }
+        })
+        .catch((res) => {
+          alert(res.data.message);
+        });
+    }
+  }
+})
+  .mount('#app');
